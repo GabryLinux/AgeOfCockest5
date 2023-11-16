13cd61414b4da0ddbefa319f7288e001d7c2a86d
GabryLinux <gabryfavazzi@gmail.com>
2023-11-16 17:20:44 +0100

    Aggiunta MAPPA + Input Handling

--- gamegraphic/characters/script/Cubo.gd
+++ gamegraphic/characters/script/Cubo.gd
@@ -21,6 +21,7 @@ func _unhandled_input(event):
 	
 	var ar = InterpoledTranslation.CoordsToVectors(coords)
 	if (
+		event is InputEventMouseButton and
 		event.is_pressed() and 
 		event.button_index == MOUSE_BUTTON_LEFT
 	):
