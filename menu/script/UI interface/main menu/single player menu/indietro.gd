4d1f68a67f5687e3372f63e3dc12448974ca341e
GabryLinux <gabryfavazzi@gmail.com>
2023-11-19 19:13:29 +0100

    Sistemati i path per navigazione tra menu

--- menu/script/UI interface/main menu/single player menu/indietro.gd
+++ menu/script/UI interface/main menu/single player menu/indietro.gd
@@ -3,4 +3,4 @@ extends Button
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://UI interface/main menu/Main menu.tscn")
+	get_tree().change_scene_to_file("res://menu/script/UI interface/main menu/Main menu.tscn")
