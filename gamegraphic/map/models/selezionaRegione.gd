f90e93829684a4d14b60479ad289029d13cd5514
GabryLinux <gabryfavazzi@gmail.com>
2023-11-19 21:50:47 +0100

    Aggiunta Mesh Manipulation + Glowing Effect

--- gamegraphic/map/models/selezionaRegione.gd
+++ gamegraphic/map/models/selezionaRegione.gd
@@ -1,11 +1,31 @@
-extends Node3D
+extends MeshInstance3D
 
 
-var mesh 
+const FEATURE_EMISSION = 1
+
+func animateGlowingEffect(material):
+	const DURATION = 0.5
+	const PROPERTY = "emission"
+	var colorStart = material.get_emission()
+	var colorEnd = Color(0.8,0.8,0.8,1)
+	material.emission_enabled = true
+	#material.set_emission(Color(1,1,1,1))
+	#material.set_emission_intensity(1)
+	
+	var tween = create_tween().set_loops()
+	
+	tween.tween_property(material, PROPERTY, colorEnd, DURATION)
+	tween.tween_property(material, PROPERTY, colorStart, DURATION)
+	#tween.start()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#mesh = = self.get_node(".").get_mesh
+
+	var material = mesh.surface_get_material(32)
+	animateGlowingEffect(material)
+
+	#print()
 	pass # Replace with function body.
 
 
